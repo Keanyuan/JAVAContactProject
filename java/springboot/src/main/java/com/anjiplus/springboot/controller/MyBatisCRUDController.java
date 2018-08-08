34eb910d455efe7debe4b4b95183be24a017bb7f
qizhiyuan <617293977@qq.com>
2018-08-08 23:21:06 +0800

    更新配置

--- java/springboot/src/main/java/com/anjiplus/springboot/controller/MyBatisCRUDController.java
+++ java/springboot/src/main/java/com/anjiplus/springboot/controller/MyBatisCRUDController.java
@@ -5,6 +5,7 @@ import com.anjiplus.springboot.service.UserService;
 import com.anjiplus.springboot.utils.AjJSONResult;
 import org.n3r.idworker.Sid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +21,7 @@ public class MyBatisCRUDController {
     @Autowired
     private Sid sid;
 
+    @RequestMapping("/saveUser")
     public AjJSONResult saveUser() throws Exception {
         String userId = sid.nextShort();
         SysUser user = new SysUser();
@@ -35,4 +37,16 @@ public class MyBatisCRUDController {
         return AjJSONResult.ok("保存成功");
     }
 
+    @RequestMapping("/updateUser")
+    public AjJSONResult updateUser(){
+        SysUser user = new SysUser();
+        user.setId("1000001");
+        user.setUsername("10011001-updated" + new Date());
+        user.setNickname("10011001-updated" + new Date());
+        user.setPassword("10011001-updated");
+        user.setIsDelete(0);
+        user.setRegistTime(new Date());
+        userService.updateUser(user);
+        return  AjJSONResult.ok("更新成功");
+    }
 }
