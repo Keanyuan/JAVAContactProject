44af9b5f34c141b546f81c348f2384771f8420e0
qizhiyuan <qizhiyuan@anji-plus.com>
2019-04-16 10:37:38 +0800

    添加 flex 布局

--- web/AwesomePos/src/router/index.js
+++ web/AwesomePos/src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Pos from '@/components/page/Pos'
 import Page from '@/components/page/Page'
 import Error from '@/components/page/Error'
+import FlexBox from '@/components/page/FlexBox'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/page/:pageName',
       name: 'Page',
       component: Page
+    },
+    {
+      path: '/FlexBox',
+      name: 'FlexBox',
+      component: FlexBox
     },
     {
       //404页面的处理
